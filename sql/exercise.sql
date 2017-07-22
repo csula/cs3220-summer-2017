73fda09b45fedb1ed596618effea102bc1c01505
Eric Liao <rcliao01@gmail.com>
2017-07-22 15:44:18 -0700

    chore: add more example solutions

--- sql/exercise.sql
+++ sql/exercise.sql
@@ -42,3 +42,31 @@ SELECT DISTINCT last_name FROM employees
 WHERE id < 10
 ORDER BY last_name;
 
+-- 2. find the id of Jane Doe
+SELECT id FROM employees
+WHERE first_name = 'Jane' AND last_name = 'doe';
+
+-- 3. find the name of employees who do not
+-- have supervisor (concatenate the first name
+-- and last name)
+SELECT CONCAT(first_name, ' ', last_name) FROM employees
+WHERE supervisor_id IS NULL;
+
+
+-- 4. List the employees who work on the project with id=1
+SELECT * FROM employees INNER JOIN project_members ON member_id = employees.id
+WHERE project_id = 1;
+-- 5. List the employees who work on the project "Blue"
+SELECT * FROM employees
+    INNER JOIN project_members ON member_id = employees.id
+    INNER JOIN projects ON project_id = projects.id
+WHERE name = 'Blue';
+-- 6. Find the name of Jane Doe's supervisor
+SELECT s.first_name, s.last_name FROM employees e
+INNER JOIN employees s ON e.supervisor_id = s.id
+WHERE e.first_name = 'Jane' AND e.last_name = 'Doe';
+
+-- 8. Find the number of employees whoes last name is "Doe"
+SELECT COUNT(*) FROM employees
+WHERE last_name = 'Doe';
+
