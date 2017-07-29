828305c088eb44f64980205bda24f24afcc16891
Eric Liao <rcliao01@gmail.com>
2017-07-28 23:54:26 -0700

    feat: add hw4, lab6 requirements

---
+++ src/main/java/edu/csula/cs3220/dao/DAO.java
@@ -0,0 +1,27 @@
+package edu.csula.cs3220.dao;
+
+import java.util.List;
+
+/**
+ * DAO (Data Access Object) is common abstraction layer between business model
+ * to database
+ *
+ * Commoon operations in this layer is to parse result set into business model
+ * and vice versa
+ */
+public interface DAO<T> {
+    // List a list of objects
+    public List<T> list();
+
+    // return single object given its id
+    public T get(int id);
+
+    // add item obj into database
+    public void add(T obj);
+
+    // update item
+    public void update(T obj);
+
+    // delete item given id
+    public void delete(int id);
+}
